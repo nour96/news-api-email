984dd929b23ca7a258452ff61d4a9bafc7778dd2
Jaafar Hamdan <hamdan@Jaafars-MacBook-Pro.local>
2025-02-27 16:14:56 +0300

    Access the news titles and discriptions

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+api_key = "ad5ff7abc3c04002aeb98b1c5dfd81c1"
+url = (
+    "https://newsapi.org/v2/everything?q=tesla&"
+    "from=2025-01-27&sortBy=publishedAt&apiKey="
+    "ad5ff7abc3c04002aeb98b1c5dfd81c1"
+)
+request = requests.get(url)
+content = request.json()
+
+for article in content["articles"]:
+    print(article["title"])
+    print(article["discription"])
+
+
