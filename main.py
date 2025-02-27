6712cc1c64418bb23779d4bc7e5ecaba4f28ec57
Jaafar Hamdan <hamdan@Jaafars-MacBook-Pro.local>
2025-02-27 23:57:14 +0300

    Send an email with titles and descriptions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests
+import requests, smtplib, ssl
 
 api_key = "ad5ff7abc3c04002aeb98b1c5dfd81c1"
 url = (
@@ -9,8 +9,25 @@ url = (
 request = requests.get(url)
 content = request.json()
 
+body = " "
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] and article["description"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2 * "\n"
+    
 
+def send_email(message):
+    host= "smtp.gmail.com"
+    port = 465
+    
+    username = "nour.ham96@gmail.com"
+    password = "ttji euxy nwxd jfaq"
 
+    receiver = "nour.ham96@gmail.com"
+    context = ssl.create_default_context()
+    
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+        
+body = body.encode("utf-8")
+send_email(body)        
